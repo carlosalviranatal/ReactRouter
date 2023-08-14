664ba3d15e789efe2fa330f1354b064ba4ab6114
carlosalviranatal <carlosalviranatal@DESKTOP-A4A091C.localdomain>
2023-08-14 09:26:42 -0500

    added stretch goals and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import './App.css'
-import Blue from './components/Blue' 
+import Blue from './components/Blue'
 import Red from './components/Red'
 import Home from './components/Home'
 import { Routes, Route, Link } from 'react-router-dom'
+import Rainbow from './components/Rainbow'
 
 function App() {
   return (
     <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar"><Link to='/blue'>Blue</Link>
-      <Link to='/red'>Red</Link>
+      <div id="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/rainbow">Rainbow</Link>
       </div>
       <div id="main-section">
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path="/blue" element= {<Blue/>} />
-          <Route path="/red" element={<Red/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
+          <Route path="/rainbow" element={<Rainbow />} />
         </Routes>
+      </div>
+      <div id='footer'>
+      <Link to="/">Home</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/rainbow">Rainbow</Link>
       </div>
     </div>
   )
---
+++ src/components/Rainbow.jsx
@@ -0,0 +1,7 @@
+export default function Rainbow() {
+    return (
+      <div className="rainbow">
+        <h1>RAINBOW</h1>
+      </div>
+    );
+  }
